52b42425b1e3746f2e1e5a621ec13b4b59a86374
Alexander Stekelenburg <a.v.stekelenburg@utwente.nl>
2024-05-22 10:03:23 +0200

    Attempting a "dumb" optimisation of ScopedStack

--- src/hre/hre/util/ScopedStack.scala
+++ src/hre/hre/util/ScopedStack.scala
@@ -1,5 +1,6 @@
 package hre.util
 
+import java.util.concurrent.atomic.AtomicBoolean
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
@@ -15,22 +16,27 @@ case object ScopedStack {
 
 case class ScopedStack[T]() {
   private val stacks: ThreadLocal[mutable.Stack[T]] = ThreadLocal.withInitial(() => mutable.Stack())
+  private val modified: AtomicBoolean = new AtomicBoolean(false)
 
-  def stack: mutable.Stack[T] = stacks.get()
+  private def stack: mutable.Stack[T] = stacks.get()
 
   def isEmpty: Boolean = stack.isEmpty
   def nonEmpty: Boolean = stack.nonEmpty
-  def push(t: T): Unit = stack.push(t)
+  def push(t: T): Unit = {
+    modified.set(true)
+    stack.push(t)
+  }
   def pop(): T = stack.pop()
   def top: T = stack.top
   def topOption: Option[T] = stack.headOption
   def find(f: T => Boolean): Option[T] = stack.find(f)
   def exists(f: T => Boolean): Boolean = stack.exists(f)
   def foreach(f: T => Unit): Unit = stack.foreach(f)
-  def toSeq: Seq[T] = stack.toSeq
+  def toSeq: Seq[T] = if (modified.get()) stack.toSeq else Seq()
   def contains(t: T): Boolean = find(_ == t).isDefined
 
   def having[R](x: T)(f: => R): R = {
+    modified.set(true)
     stack.push(x)
     try {
       f
